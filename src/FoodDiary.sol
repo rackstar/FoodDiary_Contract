564c61a0c458184227594abc95123ef6676a5629
Rocky Murdoch <rockymurdoch.tech@gmail.com>
2023-08-16 13:37:23 +0100

    feat: Add getAverageCalories method

--- src/FoodDiary.sol
+++ src/FoodDiary.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.19;
 
-import "forge-std/console2.sol";
-
 contract FoodDiary {
     event AddFoodEntry(address indexed user, bytes32 foodName, uint24 calories, uint32 timestamp);
 
@@ -18,11 +16,11 @@ contract FoodDiary {
     }
 
     address public admin;
-    uint16 public constant defaultDailyCalorieThreshold = 2100;
+    uint16 public constant DEFAULT_DAILY_CALORIE_THRESHOLD = 2100;
     uint32[] private _foodEntryTimestamps; // uint32 will let 8 timestamp entries be packed into a slot
 
-    mapping(address => FoodEntry[]) usersFoodEntries;
-    mapping(address => uint256) usersDailyCalorieThreshold;
+    mapping(address => FoodEntry[]) private usersFoodEntries;
+    mapping(address => uint256) private usersDailyCalorieThreshold;
 
     constructor() {
         // Set the contract initiator as the admin of the contract.
@@ -48,20 +46,58 @@ contract FoodDiary {
         return usersFoodEntries[msg.sender];
     }
 
+    function getUserDailyCalorieThreshold(address _user) external view returns (uint256) {
+        uint256 userDailyCalorieThreshold = usersDailyCalorieThreshold[_user];
+        if (userDailyCalorieThreshold == 0) {
+            // not set - return default value
+            return DEFAULT_DAILY_CALORIE_THRESHOLD;
+        } else {
+            return userDailyCalorieThreshold;
+        }
+    }
+
     // Admin only external methods
 
     function setUserDailyCalorieThreshold(address _user, uint256 _dailyCalorieThreshold) external onlyAdmin {
         usersDailyCalorieThreshold[_user] = _dailyCalorieThreshold;
     }
 
-    function getUserDailyCalorieThreshold(address _user) external view returns (uint256) {
-        uint256 userDailyCalorieThreshold = usersDailyCalorieThreshold[_user];
-        if (userDailyCalorieThreshold == 0) {
-            // not set - return default value
-            return defaultDailyCalorieThreshold;
-        } else {
-            return userDailyCalorieThreshold;
+    function getAverageCalories(address _user, uint256 _daysAgo) external view onlyAdmin returns (uint256) {
+        // cache the food entries
+        FoodEntry[] memory userFoodEntries = usersFoodEntries[_user];
+        if (userFoodEntries.length == 0) {
+            return 0;
         }
+
+        uint256 totalCalories = 0;
+        // Slot packing
+        uint128 numberOfEntries = 0;
+        uint128 timestampLimit = uint128(block.timestamp - (_daysAgo * 1 days) - 1); // again -1 lets us avoid >=
+
+        // loop backwards to save unnecessary iteration
+        for (uint256 i = userFoodEntries.length - 1; i >= 0;) {
+            // remove over/underflow check to save gas
+            unchecked {
+                if (userFoodEntries[i].timestamp > timestampLimit) {
+                    totalCalories += userFoodEntries[i].calories;
+                    numberOfEntries += 1;
+                } else {
+                    // older than limit
+                    break;
+                }
+                if (i == 0) {
+                    break; // weve reached the end of array
+                }
+                i--;
+            }
+        }
+
+        // Check is numberOfEntries to prevent a division by zero error
+        if (numberOfEntries == 0) {
+            return 0;
+        }
+
+        return totalCalories / numberOfEntries;
     }
 
     function getEntriesLastTwoWeeks() external view onlyAdmin returns (uint128, uint128) {
@@ -71,7 +107,7 @@ contract FoodDiary {
         uint256 oneWeekAgo = block.timestamp - 1 weeks - 1 seconds;
         uint256 twoWeeksAgo = block.timestamp - 2 weeks - 1 seconds;
 
-        // looping backwards will save unnecessary iteration
+        // loop backwards to save unnecessary iteration
         for (uint256 i = _foodEntryTimestamps.length - 1; i >= 0;) {
             // disable overflow check to save gas (since 0.8.0 arithmetic operations are now checked for overflows)
             unchecked {
@@ -86,6 +122,9 @@ contract FoodDiary {
                 } else {
                     // older than 2 weeks ago
                     break;
+                }
+                if (i == 0) {
+                    break; // weve reached the end of array
                 }
                 i--;
             }
